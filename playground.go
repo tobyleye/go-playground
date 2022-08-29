e7a3f590fa0625a6f2a5735d161fe0aa8f31ca33
Oluwatobi <krisella74@gmail.com>
2022-08-29 17:26:25 +0100

    capstone project

--- playground.go
+++ playground.go
@@ -4,6 +4,7 @@ import (
 	"fmt"
 	"math/rand"
 	"time"
+	"strings"
 )
 
 // computer programming is all about communication 
@@ -91,31 +92,45 @@ func infiniteRepetition() {
 }
 
 func guessGame() {
+	const maxGuess = 5
+
 	var game = func () {
 		rand.Seed(time.Now().UnixNano())
 		var luckyNumber = rand.Intn(100)
-		fmt.Println("\n\nWelcome to guess a number\n\n")
+		var nguesses = 0
+
+
 		for {
 			var guess int
 			fmt.Scanln(&guess)
-			fmt.Println("your guess is", guess)
+			nguesses++
 			if guess > luckyNumber {
-				fmt.Println("your guess is too high")
+				fmt.Printf("Trial %v: your guess is too high", nguesses)
 			} else if guess < luckyNumber {
-				fmt.Println("your guess is too low")
+				fmt.Printf("Trial %v: your guess is too low", nguesses)
 			} else if  guess == luckyNumber {
-				fmt.Println("Congrats you guessed the number")
+				fmt.Printf("Trial %v: Congrats you guessed the number", nguesses)
 				break
+			}	
+
+			fmt.Println()
+
+			if nguesses == maxGuess {
+				fmt.Println("You have exeeded your max no. of guesses. The answer is", luckyNumber)
+				break;
 			}
+
 		}
 		
 		fmt.Println("Thats the end of the game, thank you for playing!")
-	}
+	}	
+
+	fmt.Println("\n\nWelcome to guess a number, the game!\n\n")
 
 	for {
+		// start game
 		game()
-
-		fmt.Println("Press y/n to continue to stop playing")
+		fmt.Println("Press y/n to continue or stop playing")
 		var decision string
 		fmt.Scanln(&decision)
 
@@ -130,12 +145,124 @@ func guessGame() {
 	}
 }
 
-// func scope() {
-// 	var scopeVar = 2000
-// 	func innerScope() {
-// 		fmt.Print("scope variable", scopeVar)
-// 	}
-// }
+func scope() {
+	// var scopeVar = 2000
+
+	// short declaration 
+	// scopeVar := 2000
+	// the short declaration style is more concise and save some typing
+	// it can also be used where the var declaration can't be used.
+
+	for count := 100; count > 0; count-- {
+		fmt.Println("count = ", count)
+	}
+
+	if condition := true; condition == true {
+		fmt.Println("condition is true")
+	} else {
+		fmt.Println("condition is false")
+	}
+
+	// fmt.Println("final count is ", count)
+	
+}
+
+var era =  "AD"
+
+func scopeExample() {
+	year := rand.Intn(100) + 2000 // random year between 2000 & 2100
+	month := rand.Intn(12)
+	var daysInMonth int;
+	isLeapYear := year % 4 == 0
+
+	switch month  {
+	case 2:
+		if isLeapYear {
+			daysInMonth = 29
+		} else {
+			daysInMonth = 28
+		}
+	case 4,6,9,11:
+		daysInMonth = 30
+	default:
+		daysInMonth = 31
+	}
+	day := rand.Intn(daysInMonth) +1
+	fmt.Println(era, year, month, day)
+
+	// generally any opening curly braces introduces a new scope that ends 
+	// with a closing braces. Any variable declared directly in the scope 
+	// will be accessible throughout the span of that scope
+
+	// case & default also introduces scope although they do no not use braces
+}
+
+
+func ticketToMars() {
+	// ticket to mars
+
+	speedMin := 16
+	speedMax := 30
+	// distance in km
+	distance := 62100000
+	priceMin := 36
+	priceMax := 50
+	// departureDate := "October 13, 2020"
+
+	fmt.Printf("%-20v %-12v %-16v %4v\n", "Spaceline", "Days", "Trip type", "Price")
+	fmt.Println(strings.Repeat("=", 56))
+
+	for i:=0; i<10; i++ {
+		rand.Seed(time.Now().UnixNano())
+		var spaceline string
+
+		speed := speedMin + rand.Intn(speedMax-speedMin)
+		duration := distance / speed
+
+		durationInDays := duration / (60 * 60 * 24)	
+
+		price := priceMin + rand.Intn(priceMax-priceMin)
+
+		var tripType string
+		
+		if trip := rand.Float32(); trip <= 0.5 {
+			tripType = "One-way"
+		} else if trip > 0.5 {
+			tripType = "Round-trip"
+			price *= 2
+		}
+
+		switch randomSpaceline := rand.Intn(3); randomSpaceline {
+		case 0:
+			spaceline = "Space Adventures"
+		case 1:
+			spaceline = "SpaceX"
+		case 2:
+			spaceline = "Virgin Galactic"
+		}
+
+		fmt.Printf("%-20v %-12v %-16v $%3v\n", spaceline, durationInDays, tripType, price)
+
+	}
+	
+}
+
+func formats() {
+	fmt.Printf("%-20v $%6v\n", "Oyeleye Oluwatobi", 52500)
+}
+
+
+func loops() {
+	i := 0
+	for {
+		fmt.Println("hello world", i)
+		i++
+
+		if i == 5 {
+			break;
+		}
+	}
+}
 
 func main() {
 	// greet()
@@ -144,9 +271,13 @@ func main() {
 	// numbers()
 	// conditionals()
 	// repetition()
-	// scope()
 	// infiniteRepetition()
-	guessGame()
+	// loops()
+	// guessGame()
+	// scope()
+	// scopeExample()
+	// formats()
+	ticketToMars()
 }
 
 
